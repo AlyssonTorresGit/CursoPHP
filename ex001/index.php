58f823ac90a5597d799be9fc9b15ef207ebdcdd4
Alysson Torres <alysson-b@hotmail.com>
2025-03-18 15:49:34 -0300

    ex001

--- ex001/index.php
+++ ex001/index.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dados do Servidor</title>
 </head>
 <body>
+    <h1>dados do servidor</h1>
+    <?php 
+    phpinfo();
+    ?>
     
 </body>
 </html>
