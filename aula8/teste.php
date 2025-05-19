3952827fdf93e0449f2a811c3caef780e45dabfd
Alysson Torres <alysson-b@hotmail.com>
2025-05-19 11:56:10 -0300

    dgdfg

--- aula8/teste.php
+++ aula8/teste.php
@@ -4,15 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <?php 
-$nome = $_GET['nome'];
-$ano = $_GET['ano'];
+$nome = isset($_GET['nome']) ? $_GET['nome'] : 'não informado';
+$ano = isset($_GET['ano']) ? $_GET['ano'] : '0';
 $sexo = $_GET['sexo'];
-$idade = $_GET['idade'];
-echo "$nome e $sexo e tem $idade anos.";
+$idade = intval(date("Y")) - intval($ano);
+echo "$nome é $sexo e tem $idade anos.";
 ?>
 
+<a href="index.html">Voltar</a>
 </body>
 </html>
