c2c849c67e3e1b2f6c8e229bb57b13b91759193c
Alysson Torres <alysson-b@hotmail.com>
2025-05-20 15:22:17 -0300

    sdfgdfg

--- aula10/teste.php
+++ aula10/teste.php
@@ -8,36 +8,6 @@
 </head>
 <body>
 <?php 
-// $n = isset($_GET["num"]) ? $_GET['num'] : 0;
-// $o = isset($_GET['oper']) ? $_GET['oper'] : 1;
-// switch ($o) {
-//     case 1: 
-//         $r = $n * 2;
-//         break;
-//     case 2:
-//         $r = $n ** 3;
-//         break;
-//     case 3:
-//         $r = sqrt($n);
-// }
-
-// echo "o valor é $r";
-$d = isset($_GET['ds']) ? $_GET['ds'] : 0 
-switch ($d) {
-    case 2:
-    case 3: 
-    case 4:
-    case 5:
-    case 6:
-        echo "levanta e vai estudar";
-        break;
-    case 7:
-    case 8:
-        echo "descanse";
-        break;
-    default:
-        echo "dia da semana invalido";
-}
 
 
 
